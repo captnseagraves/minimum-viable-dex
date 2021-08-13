b4d853193c91ece039ecb1b9f90034b4ff69b83e
captnseagraves <k.s.seagraves@gmail.com>
2021-08-13 16:44:37 -0600

    implemented init() and jamming with Austin on Telegram

--- packages/buidler/contracts/DEX.sol
+++ packages/buidler/contracts/DEX.sol
@@ -8,8 +8,20 @@ contract DEX {
   using SafeMath for uint256;
   IERC20 token;
 
+  uint256 public totalLiquidity;
+
+  mapping (address => uint256) public liquidity;
+
   constructor(address token_addr) public {
     token = IERC20(token_addr);
+  }
+
+  function init() public payable returns (uint256) {
+    require(totLiquidity == 0, "DEX:init - already has liquidity");
+    totalLiquidity = address(this).balance;
+    liquidity[msg.sender] = totalLiquidity;
+    require(token.transferFrom(msg.sender, address(this), tokens));
+    return totalLiquidity;
   }
 
 }
