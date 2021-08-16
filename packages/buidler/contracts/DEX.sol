c0f2cc19f2ffd62ffc9c68d50d2ecd066f4dc466
captnseagraves <k.s.seagraves@gmail.com>
2021-08-16 11:09:45 -0600

    implement price() and ethToToken()

--- packages/buidler/contracts/DEX.sol
+++ packages/buidler/contracts/DEX.sol
@@ -16,12 +16,25 @@ contract DEX {
     token = IERC20(token_addr);
   }
 
-  function init() public payable returns (uint256) {
-    require(totLiquidity == 0, "DEX:init - already has liquidity");
+  function init(uint256 tokens) public payable returns (uint256) {
+    require(totalLiquidity == 0, "DEX:init - already has liquidity");
     totalLiquidity = address(this).balance;
     liquidity[msg.sender] = totalLiquidity;
     require(token.transferFrom(msg.sender, address(this), tokens));
     return totalLiquidity;
   }
 
+  function price(uint256 input_amount, uint256 input_reserve, uint256 output_reserve) public view returns (uint256) {
+    uint256 input_amount_with_fee = input_amount.mul(997);
+    uint256 numerator = input_amount_with_fee.mul(output_reserve);
+    uint256 denominator = input_reserve.mul(1000).add(input_amount_with_fee);
+    return numerator / denominator;
+  }
+
+  function ethToToken() public payable returns(uint256) {
+    uint256 token_reserve = token.balanceOf(address(this));
+    uint256 tokens_bought = price(msg.value, address(this).balance.sub(msg.value), token_reserve);
+    require(token.transfer(msg.sender, tokens_bought));
+    return tokens_bought;
+  }
 }
--- packages/buidler/scripts/deploy.js
+++ packages/buidler/scripts/deploy.js
@@ -15,13 +15,13 @@ async function main() {
   const dex = await deploy("DEX",[balloons.address])
 
   // paste in your address here to get 10 balloons on deploy:
-  await balloons.transfer("0x2d0B23210A6E04727842fD341Aefd5318C8eBC70",""+(10*10**18))
+  await balloons.transfer("0x037899482fbfE991e80eaEF9FB980bf2992958Fd",""+(10*10**18))
 
   // uncomment to init DEX on deploy:
-  //console.log("Approving DEX ("+dex.address+") to take Balloons from main account...")
-  //await balloons.approve(dex.address,ethers.utils.parseEther('100'))
-  //console.log("INIT exchange...")
-  //await dex.init(ethers.utils.parseEther('5'),{value:ethers.utils.parseEther('5')})
+  console.log("Approving DEX ("+dex.address+") to take Balloons from main account...")
+  await balloons.approve(dex.address,ethers.utils.parseEther('100'))
+  console.log("INIT exchange...")
+  await dex.init(ethers.utils.parseEther('5'),{value:ethers.utils.parseEther('5')})
 
 }
 main()
